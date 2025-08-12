9072bc22836617f8e0b31b83b66961388b559bbb
ArloysMacias <arloysmaciasrojas@gmail.com>
2025-08-12 09:26:32 +0000

    Secure Google Maps API key with environment variable

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+// window.appConfig = {
+//   API_KEY: 'AIzaSyA6aJC8qxLMuctL0B1OB33FfastaJ1Y-54'
+// };
+//onst API_KEY = 'AIzaSyA6aJC8qxLMuctL0B1OB33FfastaJ1Y-54';
+window.API_KEY = 'AIzaSyA6aJC8qxLMuctL0B1OB33FfastaJ1Y-54';
